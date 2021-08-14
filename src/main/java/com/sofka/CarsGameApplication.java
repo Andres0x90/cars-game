a16a302f7862caeb43c5987e636176e508a80580
Andres Cano <andrescaza246@hotmail.com>
2021-08-13 21:21:39 -0500

    Proyecto creado y funcionalidad crear juego parcialmente terminada

---
+++ src/main/java/com/sofka/CarsGameApplication.java
@@ -0,0 +1,13 @@
+package com.sofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsGameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sofka/controllers/GameController.java
@@ -0,0 +1,20 @@
+package com.sofka.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GameController 
+{
+	@GetMapping({"/", ""})
+	public String game(@RequestParam(required = false) Integer players,Model model) 
+	{
+		model.addAttribute("players",players);
+		return "create";
+	}
+	
+	
+}
